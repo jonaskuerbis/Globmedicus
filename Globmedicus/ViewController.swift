020b2e0be9be017098f6517f63766469ec92d16b
Jonas@kuerbis.name <jonas@kuerbis.name>
2018-12-08 20:31:08 +0100

    Initial Commit

---
+++ Globmedicus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Globmedicus
+//
+//  Created by Jonas Stefan Kürbis on 12/8/18.
+//  Copyright © 2018 Jonas Stefan Kürbis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
