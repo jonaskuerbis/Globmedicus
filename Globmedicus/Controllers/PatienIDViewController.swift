b379e3c09681d4baa0385a08fb8dbab89ea7f2b9
Jonas@kuerbis.name <jonas@kuerbis.name>
2018-12-10 10:17:14 +0100

    First Try

---
+++ Globmedicus/Controllers/PatienIDViewController.swift
@@ -0,0 +1,145 @@
+//
+//  PatienIDViewController.swift
+//  Globmedicus
+//
+//  Created by Jonas Stefan Kürbis on 12/9/18.
+//  Copyright © 2018 Jonas Stefan Kürbis. All rights reserved.
+//
+
+import UIKit
+
+class PatienIDViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Patients.plist")
+    
+   
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        
+        print(dataFilePath)
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        
+        
+        
+        
+        
+        
+//        if let items = defaults.array(forKey: "PatientListArray") as? [Item] {
+//            itemArray = items
+        
+            
+//            // Do any additional setup after loading the view.
+    }
+        
+        
+        //Mark - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
+        }
+        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+            
+            let item = itemArray[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            
+            // Ternary operator
+            // value = condition ?
+            
+            cell.accessoryType = item.done ? .checkmark : .none
+            
+            
+            
+            return cell
+            
+        }
+        
+        //Mark - TableView - Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            //        print(itemArray[indexPath.row])
+            
+            // Accessory
+            
+            
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+            
+            
+            
+            tableView.reloadData()
+            tableView.deselectRow(at: indexPath, animated: true)
+        // Hier könnte man einen Button einfügen
+        }
+        
+        
+    
+        
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+    
+    
+    
+            
+            var textField = UITextField()
+            
+            let alert = UIAlertController(title: "Add New Patient", message: "", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Add Patient", style: .default) { (action) in
+                // what will happen once the surgeon clicks the Add Patient Item
+                let newItem = Item()
+                newItem.title = textField.text!
+                
+                self.itemArray.append(newItem)
+                
+                let encoder = PropertyListEncoder()
+                
+                do {
+                  let data = try encoder.encode(self.itemArray)
+                    try data.write(to: self.dataFilePath!)
+                }catch{
+                    print("Error encoding Item Array\(error)")
+                }
+                
+                
+                
+                
+                self.tableView.reloadData()
+        
+                
+            }
+            
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Add New Patient iD"
+                print(alertTextField.text)
+                textField = alertTextField
+            }
+            
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            
+            
+        }
+        
+
+    
+    
+
+}
---
+++ Globmedicus/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Globmedicus
+//
+//  Created by Jonas Stefan Kürbis on 12/9/18.
+//  Copyright © 2018 Jonas Stefan Kürbis. All rights reserved.
+//
+
+import Foundation
+
+class Item: Encodable {
+    var title: String = ""
+    var done: Bool = false
+}
